52f51ef0b59a809bf890b021901cdd0767c35880
Henry Novoa <henry.r.novoa@gmail.com>
2020-11-12 21:35:54 +0100

    add logger

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 // capture the environment variables
 
-const { DATA_URL, API_PORT, JWT_SECRET, CLIENT_ENDPOINT, POLICY_ENDPOINT } = process.env
+const { DATA_URL, API_PORT, JWT_SECRET, CLIENT_ENDPOINT, POLICY_ENDPOINT, HOST } = process.env
 
 // Validate the required config information
 assert(DATA_URL, 'DATA_URL is required')
@@ -14,11 +14,13 @@ assert(API_PORT, 'API_PORT is required')
 assert(JWT_SECRET, 'JWT_SECRET is required')
 assert(CLIENT_ENDPOINT, 'CLIENT_ENDPOINT is required')
 assert(POLICY_ENDPOINT, 'POLICY_ENDPOINT is required')
+assert(HOST, 'LOCALHOST is required')
 
 module.exports = {
   DATA_URL,
   API_PORT,
   JWT_SECRET,
   CLIENT_ENDPOINT,
-  POLICY_ENDPOINT
+  POLICY_ENDPOINT,
+  HOST
 }
--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const config = require('./config')
 const packageJSON = require('./package.json')
-const { API_PORT } = config
+const { API_PORT, HOST } = config
+const morgan = require('morgan')
+const winston = require('winston')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+
+// Create logger
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'server.log' })
+    ]
+})
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+app.use(morgan('combined', { stream: accessLogStream }))
+
+
+
+
 // Upload the routes
 const {
   user,
   policy
 } = require('./src/routes')
-const app = express()
+
 
 // Define global middleware
 app.use(express.json())
@@ -16,6 +39,12 @@ app.use(express.json())
 app.use('/api', user)
 app.use('/api', policy)
 
-app.listen(API_PORT, () => {
-  console.log(`${packageJSON.name} ${packageJSON.version} up and running on http://localhost:${API_PORT}`)
+// Log if the server has been successfully initiated
+app.listen(API_PORT, HOST, () => logger.info(`${packageJSON.name} ${packageJSON.version} running and listening at http://${HOST}:${API_PORT}/api`))
+
+// Log if the server is abruptly disconnected
+process.on('SIGINT', () => {
+    logger.info('Server abruptly stopped')
+
+    process.exit(0)
 })
